7b800ff40db02466080bb449e7cec0b155342cf4
Laurelthrone <laurelthrone@gmail.com>
2020-09-22 22:14:50 -0700

    water generation + futureproofing

--- Assets/WorldGen.cs
+++ Assets/WorldGen.cs
@@ -9,6 +9,7 @@ public class WorldGen : MonoBehaviour
     public Grid tileGrid, moveGrid;
     GridManager tileGMgr, moveGMgr;
     public Tile ground,
+                ////////////////////////////////////////
                 grassTLC, grassT, grassTRC,
                 grassL, grassM, grassR,
                 grassBLC, grassB, grassBRC,
@@ -16,21 +17,80 @@ public class WorldGen : MonoBehaviour
                 grassIBL, grassIBR, grassITL, grassITR,
 
                 grass2CL, grass2CR, grass2CU, grass2CD,
-                grass1L, grass1R, grass1U, grass1D;
+                grass1L, grass1R, grass1U, grass1D,
+                ////////////////////////////////////////
+                waterTLC, waterT, waterTRC,
+                waterL, waterM, waterR,
+                waterBLC, waterB, waterBRC,
 
-    public Tilemap groundMap, grassMap, obstacleMap;
+                waterIBL, waterIBR, waterITL, waterITR,
+
+                water2CL, water2CR, water2CU, water2CD,
+                water1L, water1R, water1U, water1D;
+                ////////////////////////////////////////
+
+    public Tilemap groundMap, grassMap, obstacleMap, waterMap, activeMap;
     const int SIZE = 20, MAX_STEPS = 6;
     Vector3Int currentPos;  
-    public static List<Vector3Int> grassSeeds;
-    public static List<Vector3Int> grassBranches;
+    public static List<Vector3Int> grassSeeds, waterSeeds, activeSeeds, allOccupied;
+    public static List<Tile> grassTiles, waterTiles, activeTiles;
+
     public static bool firstRun = true;
 
     // Start is called before the first frame update
     void Start()
     {
         grassSeeds = new List<Vector3Int>();
+        waterSeeds = new List<Vector3Int>();
+        allOccupied = new List<Vector3Int>();
+        grassTiles = new List<Tile>();
+        waterTiles = new List<Tile>();
         tileGMgr = new GridManager(tileGrid);
         moveGMgr = new GridManager(moveGrid);
+
+        grassTiles.Add(grassTLC);
+        grassTiles.Add(grassT);
+        grassTiles.Add(grassTRC);
+        grassTiles.Add(grassL);
+        grassTiles.Add(grassM);
+        grassTiles.Add(grassR);
+        grassTiles.Add(grassBLC);
+        grassTiles.Add(grassB);
+        grassTiles.Add(grassBRC);
+        grassTiles.Add(grassIBL);
+        grassTiles.Add(grassIBR);
+        grassTiles.Add(grassITL);
+        grassTiles.Add(grassITR);
+        grassTiles.Add(grass2CL);
+        grassTiles.Add(grass2CR);
+        grassTiles.Add(grass2CU);
+        grassTiles.Add(grass2CD);
+        grassTiles.Add(grass1L);
+        grassTiles.Add(grass1R);
+        grassTiles.Add(grass1U);
+        grassTiles.Add(grass1D);
+
+        waterTiles.Add(waterTLC);
+        waterTiles.Add(waterT);
+        waterTiles.Add(waterTRC);
+        waterTiles.Add(waterL);
+        waterTiles.Add(waterM);
+        waterTiles.Add(waterR);
+        waterTiles.Add(waterBLC);
+        waterTiles.Add(waterB);
+        waterTiles.Add(waterBRC);
+        waterTiles.Add(waterIBL);
+        waterTiles.Add(waterIBR);
+        waterTiles.Add(waterITL);
+        waterTiles.Add(waterITR);
+        waterTiles.Add(water2CL);
+        waterTiles.Add(water2CR);
+        waterTiles.Add(water2CU);
+        waterTiles.Add(water2CD);
+        waterTiles.Add(water1L);
+        waterTiles.Add(water1R);
+        waterTiles.Add(water1U);
+        waterTiles.Add(water1D);
 
         //Generate base tiles
         for (int i = -SIZE; i < SIZE; i++)
@@ -41,21 +101,34 @@ public class WorldGen : MonoBehaviour
             }
         }
 
+        doPatchGen(grassMap, grassTiles, grassSeeds);
+        doPatchGen(waterMap, waterTiles, waterSeeds);
+
+    }
+
+    void doPatchGen(Tilemap a, List<Tile> b, List<Vector3Int> c)
+    {
+        activeMap = a;
+        Debug.Log("Map: " + activeMap.name);
+        activeTiles = b;
+        activeSeeds = c;
+
         //Generate grass seeds
         for (int i = -SIZE; i < SIZE; i++)
         {
             for (int j = -SIZE; j < SIZE; j++)
             {
-                if ((!spacing(new Vector2Int(i, j), 20) && Chance.Percent(1) && !grassMap.HasTile(new Vector3Int(i, j, 0))))
+                if ((spacing(new Vector2Int(i, j), 20) && Chance.Percent(1) && !activeMap.HasTile(new Vector3Int(i, j, 0))))
                 {
-                    grassMap.SetTile(new Vector3Int(i, j, 0), grassM);
-                    grassSeeds.Add(new Vector3Int(i, j, 0));
+                    activeMap.SetTile(new Vector3Int(i, j, 0), activeTiles[4]);
+                    activeSeeds.Add(new Vector3Int(i, j, 0));
+                    allOccupied.Add(new Vector3Int(i, j, 0));
                 }
             }
         }
 
         //Create grass branches
-        foreach (Vector3Int seedPos in grassSeeds)
+        foreach (Vector3Int seedPos in activeSeeds)
         {
             int numSteps;
             int xDir = 0;
@@ -89,7 +162,8 @@ public class WorldGen : MonoBehaviour
                 currentPos = seedPos + movMod;
                 for (int j = 0; j < numSteps; j++)
                 {
-                    grassMap.SetTile(currentPos, grassM);
+                    activeMap.SetTile(currentPos, activeTiles[4]);
+                    allOccupied.Add(currentPos);
                     currentPos += movMod;
                     if (!lastWasDiag && Chance.Percent(45))
                     {
@@ -105,7 +179,7 @@ public class WorldGen : MonoBehaviour
         }
 
         //Fill in patches
-        foreach (Vector3Int seedPos in grassSeeds)
+        foreach (Vector3Int seedPos in activeSeeds)
         {
             for (int i = 0; i < 1; i++)
             {
@@ -119,7 +193,8 @@ public class WorldGen : MonoBehaviour
                         //Fill empty tiles that have at least 3 adjacent tiles
                         if (numAdjacents(currentPos) >= 3)
                         {
-                            grassMap.SetTile(currentPos, grassM);
+                            activeMap.SetTile(currentPos, activeTiles[4]);
+                            allOccupied.Add(currentPos);
                         }
                     }
                 }
@@ -127,7 +202,7 @@ public class WorldGen : MonoBehaviour
         }
 
         //Cleanup first pass (outline)
-        foreach (Vector3Int seedPos in grassSeeds)
+        foreach (Vector3Int seedPos in activeSeeds)
         {
             for (int x = MAX_STEPS * -2; x < MAX_STEPS * 2; x++)
             {
@@ -138,8 +213,8 @@ public class WorldGen : MonoBehaviour
             }
         }
 
-        //Cleanup second pass (inner corner)
-        foreach (Vector3Int seedPos in grassSeeds)
+        //Cleanup second pass (inner corners and 1x1s)
+        foreach (Vector3Int seedPos in activeSeeds)
         {
             for (int x = MAX_STEPS * -2; x < MAX_STEPS * 2; x++)
             {
@@ -150,6 +225,7 @@ public class WorldGen : MonoBehaviour
             }
         }
     }
+
     int numAdjacents(Vector3Int pos)
     {
         int counter = 0;
@@ -158,8 +234,8 @@ public class WorldGen : MonoBehaviour
             for (int j = -1; j <= 1; j++)
             {
                 Vector3Int tspot = new Vector3Int(pos.x + i, pos.y + j, 0);
-                Debug.Log("i " + (pos.x + i) + "j " + (pos.y + j) + "pos " + tspot + "cond " + grassMap.HasTile(tspot));
-                if (grassMap.HasTile(tspot) && tspot != pos) counter++;
+                Debug.Log("i " + (pos.x + i) + "j " + (pos.y + j) + "pos " + tspot + "cond " + activeMap.HasTile(tspot));
+                if (activeMap.HasTile(tspot) && tspot != pos) counter++;
             }
         }
         return counter;
@@ -172,10 +248,10 @@ public class WorldGen : MonoBehaviour
             for (int j = 0; j < width; j++)
             {
                 Vector3Int t = new Vector3Int(pos.x - (width - 2) + i, pos.y - (width - 2) + j, 0);
-                if (grassMap.HasTile(t)) return true;
+                if (activeMap.HasTile(t) || allOccupied.Contains(t)) return false;
             }
         }
-        return false;
+        return true;
     }
 
     bool checkTile(Vector3Int a, string dir, string diag)
@@ -188,13 +264,13 @@ public class WorldGen : MonoBehaviour
         switch (dir)
         {
             case "up":
-                return grassMap.HasTile(new Vector3Int(a.x, a.y + 1, 0));
+                return activeMap.HasTile(new Vector3Int(a.x, a.y + 1, 0));
             case "down":
-                return grassMap.HasTile(new Vector3Int(a.x, a.y - 1, 0));
+                return activeMap.HasTile(new Vector3Int(a.x, a.y - 1, 0));
             case "left":
-                return grassMap.HasTile(new Vector3Int(a.x - 1, a.y + yOffset, 0));
+                return activeMap.HasTile(new Vector3Int(a.x - 1, a.y + yOffset, 0));
             case "right":
-                return grassMap.HasTile(new Vector3Int(a.x + 1, a.y + yOffset, 0));
+                return activeMap.HasTile(new Vector3Int(a.x + 1, a.y + yOffset, 0));
         }
         return false;
     }
@@ -204,13 +280,13 @@ public class WorldGen : MonoBehaviour
         switch (dir)
         {
             case "up":
-                return grassMap.HasTile(new Vector3Int(a.x, a.y + 1, 0));
+                return activeMap.HasTile(new Vector3Int(a.x, a.y + 1, 0));
             case "down":
-                return grassMap.HasTile(new Vector3Int(a.x, a.y - 1, 0));
+                return activeMap.HasTile(new Vector3Int(a.x, a.y - 1, 0));
             case "left":
-                return grassMap.HasTile(new Vector3Int(a.x - 1, a.y, 0));
+                return activeMap.HasTile(new Vector3Int(a.x - 1, a.y, 0));
             case "right":
-                return grassMap.HasTile(new Vector3Int(a.x + 1, a.y, 0));
+                return activeMap.HasTile(new Vector3Int(a.x + 1, a.y, 0));
         }
         return false;
     }
@@ -218,17 +294,19 @@ public class WorldGen : MonoBehaviour
     void determineOutline(Vector3Int a)
     {
         int adjacents = numAdjacents(a);
-        if (!grassMap.HasTile(a) || adjacents == 8) return;
+        if (!activeMap.HasTile(a) || adjacents == 8) return;
 
         //Checks sides
         if (twoVerts(a) && !twoHoriz(a))
         {
-            grassMap.SetTile(a, checkTile(a, "left") ? grassR : grassL);
+            activeMap.SetTile(a, checkTile(a, "left") ? activeTiles[5] : activeTiles[3]);
+            allOccupied.Add(a);
             return;
         }
         if (twoHoriz(a) && !twoVerts(a))
         {
-            grassMap.SetTile(a, checkTile(a, "up") ? grassB : grassT);
+            activeMap.SetTile(a, checkTile(a, "up") ? activeTiles[7] : activeTiles[1]);
+            allOccupied.Add(a);
             return;
         }
 
@@ -237,14 +315,16 @@ public class WorldGen : MonoBehaviour
         {
             if (checkTile(a, "down"))
             {
-                if (checkTile(a, "left")) grassMap.SetTile(a, grassTRC);
-                else grassMap.SetTile(a, grassTLC);
+                if (checkTile(a, "left")) activeMap.SetTile(a, activeTiles[2]);
+                else activeMap.SetTile(a, activeTiles[0]);
+                allOccupied.Add(a);
                 return;
             }
             else
             {
-                if (checkTile(a, "left")) grassMap.SetTile(a, grassBRC);
-                else grassMap.SetTile(a, grassBLC);
+                if (checkTile(a, "left")) activeMap.SetTile(a, activeTiles[8]);
+                else activeMap.SetTile(a, activeTiles[6]);
+                allOccupied.Add(a);
                 return;
             }
         }
@@ -285,33 +365,36 @@ public class WorldGen : MonoBehaviour
     void innersAnd1x1s(Vector3Int a)
     {
         //If tile exists, is not completely surrounded, and has all four cardinal directions filled, it is an inner corner piece
-        if(grassMap.HasTile(a) && numAdjacents(a) != 8 && numCardinals(a) == 4)
+        if (activeMap.HasTile(a) && numAdjacents(a) != 8 && numCardinals(a) == 4)
         {
             if (numDiagonals(a) == 2) twoCorners(a);
             else if (numDiagonals(a) == 3) oneCorner(a);
         }
-        else if(grassMap.HasTile(a) && numCardinals(a) == 1)
+        else if (activeMap.HasTile(a) && numCardinals(a) == 1)
         {
-            if (!checkTile(a, "left")) grassMap.SetTile(a, grass1L);
-            else if (!checkTile(a, "right")) grassMap.SetTile(a, grass1R);
-            else if (!checkTile(a, "up")) grassMap.SetTile(a, grass1U);
-            else if (!checkTile(a, "down")) grassMap.SetTile(a, grass1D);
+            if (!checkTile(a, "left")) activeMap.SetTile(a, activeTiles[17]);
+            else if (!checkTile(a, "right")) activeMap.SetTile(a, activeTiles[18]);
+            else if (!checkTile(a, "up")) activeMap.SetTile(a, activeTiles[19]);
+            else if (!checkTile(a, "down")) activeMap.SetTile(a, activeTiles[20]);
+            allOccupied.Add(a);
         }
     }
 
     void twoCorners(Vector3Int a)
     {
-        if (!checkTile(a, "left", "down") && !checkTile(a, "left", "up")) grassMap.SetTile(a, grass2CL);
-        else if (!checkTile(a, "left", "down") && !checkTile(a, "right", "down")) grassMap.SetTile(a, grass2CD);
-        else if (!checkTile(a, "left", "up") && !checkTile(a, "right", "up")) grassMap.SetTile(a, grass2CU);
-        else if (!checkTile(a, "right", "down") && !checkTile(a, "right", "up")) grassMap.SetTile(a, grass2CR);
+        if (!checkTile(a, "left", "down") && !checkTile(a, "left", "up")) activeMap.SetTile(a, activeTiles[13]);
+        else if (!checkTile(a, "left", "down") && !checkTile(a, "right", "down")) activeMap.SetTile(a, activeTiles[16]);
+        else if (!checkTile(a, "left", "up") && !checkTile(a, "right", "up")) activeMap.SetTile(a, activeTiles[15]);
+        else if (!checkTile(a, "right", "down") && !checkTile(a, "right", "up")) activeMap.SetTile(a, activeTiles[14]);
+        allOccupied.Add(a);
     }
 
     void oneCorner(Vector3Int a)
     {
-        if (!checkTile(a, "left", "down")) grassMap.SetTile(a, grassIBL);
-        else if (!checkTile(a, "left", "up")) grassMap.SetTile(a, grassITL);
-        else if (!checkTile(a, "right", "up")) grassMap.SetTile(a, grassITR);
-        else if (!checkTile(a, "right", "down")) grassMap.SetTile(a, grassIBR);
+        if (!checkTile(a, "left", "down")) activeMap.SetTile(a, activeTiles[9]);
+        else if (!checkTile(a, "left", "up")) activeMap.SetTile(a, activeTiles[11]);
+        else if (!checkTile(a, "right", "up")) activeMap.SetTile(a, activeTiles[12]);
+        else if (!checkTile(a, "right", "down")) activeMap.SetTile(a, activeTiles[10]);
+        allOccupied.Add(a);
     }
 }
